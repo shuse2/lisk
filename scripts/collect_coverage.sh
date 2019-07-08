fb857174cf67b675f4c824796f68a4e54a15ae12
Shusetsu Toda <shusetsu@lightcurve.io>
2019-07-08 17:48:12 +0200

    Update jenkins file

--- scripts/collect_coverage.sh
+++ scripts/collect_coverage.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 NYC_PATH=".nyc_output"
 
-rm -r "./$NYC_PATH/*"
+rm -r "./$NYC_PATH"
+rm .coverage/coverage.json
 
 # Coverage for elements
 for d in elements/*; do
 	if [ -d $d ] && [ -d $d/.nyc_output ]; then
-		cp -av $d/.nyc_output ./
+		cp -a $d/.nyc_output ./
 	fi
 done
 
 # Coverage for framework
-cp -vr ./framework/test/mocha/.coverage-unit/* ./.nyc_output
-cp -v ./.coverage/unit/coverage-final.json ./.nyc_output/coverage-framework-unit.json
+cp -r ./framework/test/mocha/.coverage-unit/* ./.nyc_output
+cp ./.coverage/unit/coverage-final.json ./.nyc_output/coverage-framework-unit.json
 
 # Coverage for commander
-cp -vr ./commander/.nyc_output/* ./.nyc_output
+cp -r ./commander/.nyc_output/* ./.nyc_output
 
 npx nyc merge ./.nyc_output .coverage/coverage.json
