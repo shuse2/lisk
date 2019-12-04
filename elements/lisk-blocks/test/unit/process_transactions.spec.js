5c04cddc42c795e5371fc1146aa0848e5ef62fa7
shuse2 <shus.toda@gmail.com>
2019-12-05 05:07:13 +0800

    :recycle: Fix migrated tests

--- elements/lisk-blocks/test/unit/process_transactions.spec.js
+++ elements/lisk-blocks/test/unit/process_transactions.spec.js
@@ -19,61 +19,59 @@ const {
 	composeTransactionSteps,
 } = require('../../src/transactions/compose_transaction_steps');
 
-describe('process_transactions', () => {
-	describe('#composeTransactionSteps', () => {
-		const transactions = [
+describe('#composeTransactionSteps', () => {
+	const testTransactions = [
+		{
+			id: 'anId',
+			matcher: () => true,
+			type: 0,
+		},
+		{
+			id: 'anotherId',
+			matcher: () => false,
+			type: 1,
+		},
+	];
+
+	const step1Response = {
+		transactionsResponses: [
 			{
-				id: 'anId',
-				matcher: () => true,
-				type: 0,
+				id: 'id1',
+				status: TransactionStatus.FAIL,
 			},
+		],
+	};
+
+	const step2Response = {
+		transactionsResponses: [
 			{
-				id: 'anotherId',
-				matcher: () => false,
-				type: 1,
+				id: 'id2',
+				status: TransactionStatus.OK,
 			},
-		];
+		],
+	};
 
-		const step1Response = {
+	const step1 = jest.fn().mockReturnValue(step1Response);
+	const step2 = jest.fn().mockReturnValue(step2Response);
+	const composedFunction = composeTransactionSteps(step1, step2);
+	let result;
+
+	beforeEach(async () => {
+		result = await composedFunction(testTransactions);
+	});
+
+	it('should return a combination of the result of executing both steps', async () => {
+		// Assert
+		expect(result).toEqual({
 			transactionsResponses: [
-				{
-					id: 'id1',
-					status: TransactionStatus.FAIL,
-				},
+				...step1Response.transactionsResponses,
+				...step2Response.transactionsResponses,
 			],
-		};
-
-		const step2Response = {
-			transactionsResponses: [
-				{
-					id: 'id2',
-					status: TransactionStatus.OK,
-				},
-			],
-		};
-
-		const step1 = sinonSandbox.stub().returns(step1Response);
-		const step2 = sinonSandbox.stub().returns(step2Response);
-		const composedFunction = composeTransactionSteps(step1, step2);
-		let result;
-
-		beforeEach(async () => {
-			result = await composedFunction(transactions);
 		});
+	});
 
-		it('should return a combination of the result of executing both steps', async () => {
-			// Assert
-			expect(result).to.deep.equal({
-				transactionsResponses: [
-					...step1Response.transactionsResponses,
-					...step2Response.transactionsResponses,
-				],
-			});
-		});
-
-		it('should only pass successfull transactions to the next step', async () => {
-			// Assert
-			expect(step2).to.have.been.calledWith([]);
-		});
+	it('should only pass successfull transactions to the next step', async () => {
+		// Assert
+		expect(step2).toHaveBeenCalledWith([], undefined);
 	});
 });
